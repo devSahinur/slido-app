a9eda530c29d8fffd1e4b5d382a486e6fa3143e9
Sahinur <sahinur@Sahinur.local>
2024-03-31 20:27:31 +0600

    [SET UP]

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+        return (
+            <h1>
+                login page
+            </h1>
+        );
+}
+export default page;
